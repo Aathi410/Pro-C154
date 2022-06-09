b623d15b84f4521c69922c44ade251b9420be65a
Aathi <n4aathi04@gmail.com>
2022-06-09 14:05:29 +0530

    corrected

--- js/Fish.js
+++ js/Fish.js
@@ -1,13 +1,13 @@
 AFRAME.registerComponent("fish", {
     init:function(){
-        for(var i = 1; i <= 10; i ++){
+        for(var i = 1; i <= 10; i++){
             const id = `fish${i}`
 
-            const posX = Math.round() * 100 + -50
-            const posY = Math.round() * 5 + 5
-            const posZ = Math.round() * 60 + -40
+            const posX = Math.random() * 100 + -50
+            const posY = Math.random() * 5 + 5
+            const posZ = Math.random() * 60 + -40
             
-            const position = { x : posX, y : posY, z : posZ }
+            const position = { x:posX, y:posY, z:posZ }
             this.createFish(id, position)
         }
     },
@@ -20,10 +20,17 @@ AFRAME.registerComponent("fish", {
         fishEl.setAttribute("id", id)
         fishEl.setAttribute("position", position)
 
-        fishEl.setAttribute("rotation", { x : 0, y : 180, z :0})
-        fishEl.setAttribute("scale", { x : 0.3, y : 0.3, z : 0.3})
+        fishEl.setAttribute("rotation", { x: 0, y: 180, z: 0 })
+        fishEl.setAttribute("scale", { x: 0.3, y: 0.3, z: 0.3 })
         
         fishEl.setAttribute("gltf-model", "./assets/fish/scene.gltf")
+
+        fishEl.setAttribute("animation", {
+            property: "position",
+            to: "100 10 -20",
+            loop: "true",
+            dur: 20000
+        })
 
         fishEl.setAttribute("animation-mixer", {})
 
